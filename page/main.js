56e0cb443b0dde0b16120a721f67bab66e10c02b
GreenSoupDev <109924369+GreenSoupDeveloper@users.noreply.github.com>
2025-01-24 19:59:51 -0400

    test

--- page/main.js
+++ page/main.js
@@ -181,3 +181,16 @@ function analizePosts(){
         
     }
 }
+function stalk(){
+   
+
+    document.cookie = "true";
+    
+}
+function deleteAllCookies() {
+    document.cookie.split(';').forEach(cookie => {
+        const eqPos = cookie.indexOf('=');
+        const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
+        document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+    });
+}
